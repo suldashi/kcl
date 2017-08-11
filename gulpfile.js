3a73fc18b0ac6274c346ab73dc70dfbfbc75f328
Ermir Suldashi <suldashi@github.com>
2017-08-11 14:05:10 +0200

    simplified the websocket library and replaced it with one that reconnects automatically when disconnected.

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,19 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var rename = require('gulp-rename');
-var del = require("del");
 var browserify = require("browserify");
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 var runSequence = require("run-sequence");
-var exec = require('child_process').exec;
 
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('server-pre', function() {
-	return gulp.src("src/ts/ws/websocketserver.ts")
-		.pipe(rename("websocket.ts"))
-		.pipe(gulp.dest("src/ts/ws/"));
-});
 
 gulp.task('server-build', function() {
-	return gulp.src(["src/ts/**/*.ts","!src/ts/ws/websocketserver.ts","!src/ts/ws/websocketbrowser.ts"])
+	return gulp.src(["src/ts/**/*.ts"])
 		.pipe(tsProject())
 		.pipe(gulp.dest("./server"));
 });
 
-gulp.task('server-post', function() {
-	return del("src/ts/ws/websocket.ts",{force:true});
-});
-
-gulp.task('browser-pre', function() {
-	return gulp.src("src/ts/ws/websocketbrowser.ts")
-		.pipe(rename("websocket.ts"))
-		.pipe(gulp.dest("src/ts/ws/"));
-});
 
 gulp.task('browser-build', function() {
 	return gulp.src(["src/ts/**/*.ts","!src/ts/ws/websocketserver.ts","!src/ts/ws/websocketbrowser.ts"])
@@ -38,9 +21,6 @@ gulp.task('browser-build', function() {
 		.pipe(gulp.dest("./tmp"));
 });
 
-gulp.task('browser-post', function() {
-	return del(["src/ts/ws/websocket.ts","tmp"],{force:true});
-});
 
 gulp.task("browserify", () => {
 	return browserify({entries:"tmp/kcl.js",debug:false}).bundle()
@@ -50,9 +30,9 @@ gulp.task("browserify", () => {
 });
 
 gulp.task("browser", () => {
-	return runSequence("browser-pre","browser-build","browserify","browser-post");
+	return runSequence("browser-build","browserify");
 });
 
 gulp.task("server", () => {
-	return runSequence("server-pre","server-build","server-post");
+	return runSequence("server-build");
 });
